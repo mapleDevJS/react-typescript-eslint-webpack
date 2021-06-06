aba72de51cfcec131002fdee291acb5b666a9724
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-06-05 22:50:27 -0400

    merge two configs into single file

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,66 +1,78 @@
-import path from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import ESLintPlugin from "eslint-webpack-plugin";
+import * as path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { Configuration, HotModuleReplacementPlugin } from 'webpack';
 
-interface WebpackConfiguration extends webpack.Configuration {
-  devServer: {
-    contentBase: string,
-    historyApiFallback: boolean,
-    port: number,
-    open: boolean,
-    hot: boolean,
-  }
-}
+const isProd = process.env.NODE_ENV === "production";
 
-const config: WebpackConfiguration = {
-  mode: "development",
+const config: Configuration = {
+  mode: isProd ? 'production' : 'development',
   output: {
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: "[name].[contenthash].js",
   },
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
             ],
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new HotModuleReplacementPlugin(),
     new ForkTsCheckerWebpackPlugin({
       async: false,
     }),
     new ESLintPlugin({
-      extensions: ["js", "jsx", "ts", "tsx"],
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
     }),
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "build"),
+    contentBase: path.join(__dirname, 'build'),
     historyApiFallback: true,
     port: 4000,
     open: true,
     hot: true,
+    compress: true,
+    watchContentBase: true,
   },
 };
+
+if (isProd) {
+    delete config.devtool;
+    delete config.devServer;
+}
 
 export default config;
