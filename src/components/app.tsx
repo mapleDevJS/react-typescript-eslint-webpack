ef6a055ae07a8b7c223dfa566a7bb3fa2bb176f7
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-06-05 22:54:30 -0400

    update starter ui components

---
+++ src/components/app.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './app.css';
+import logo from '../assets/logo.svg';
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <img src={logo} className="app__logo" alt="logo" />
+        <p>
+          Edit <code>src/app.tsx</code> and save to reload.
+        </p>
+        <a
+          className="app__link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => (
-  <h1>My React and TypeScript App!! {new Date().toLocaleDateString()}</h1>
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components/app';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
