1ee88ad147017e163d96c790bd3283a3cb85181b
Alexey Ivanov <alexeyivanov@fb.com>
2024-05-20 23:01:00 -0400

    Update package dependencies and tweak webpack configuration
    The package.json dependencies and devDependencies have been updated to their newer versions. Simultaneously, the webpack.config.ts has been adjusted to better suit the new dependencies, offering up-to-date compatibility and improved webpack configuration for managing assets.

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,75 +4,75 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import { Configuration, HotModuleReplacementPlugin } from 'webpack';
 
+interface ConfigurationDevServer extends Configuration {
+    devServer?: any;
+}
+
 const isProd = process.env.NODE_ENV === "production";
 
-const config: Configuration = {
-  mode: isProd ? 'production' : 'development',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: "[name].[contenthash].js",
-  },
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
+const config: ConfigurationDevServer = {
+    mode: isProd ? 'production' : 'development',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: isProd ? "[hash][ext][query]" : "[name][ext]",
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+        }),
+        new ESLintPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new HotModuleReplacementPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-    }),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    historyApiFallback: true,
-    port: 4000,
-    open: true,
-    hot: true,
-    compress: true,
-    watchContentBase: true,
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.join(__dirname, 'build'),
+        historyApiFallback: true,
+        port: 4000,
+        open: true,
+        hot: true,
+        watchFiles: 'src/**/*',
+    },
 };
-
 if (isProd) {
     delete config.devtool;
     delete config.devServer;
 }
-
 export default config;
